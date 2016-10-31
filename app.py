4b5aad2e3a8d6b61c8953fd37bec0e3c91719fa8
Stuart Logan <stuart@stuart-logan.com>
2016-10-31 13:53:26 +1100

    print environ

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGSg'])
 
+print(os.environ['APP_SETTINGS'])
+
 @app.route('/')
 def hello():
     return "Hello World!"
